f858315989dae3ced09ba826d3ab326306681dc4
khoitq <khoitq.1992@gmail.com>
2025-10-08 15:16:20 +0700

    Update site

---
+++ partials/home/page-video.php
@@ -0,0 +1,23 @@
+<div class="video-section wrapper">
+    <div class="container">
+        <img class="video-bg" src="<?php echo get_template_directory_uri(); ?>/assets/img/video-background.png" alt="">
+        <div class="video-gallery">
+            <div class="video-gallery-content">
+                <img class="video-gallery__bg" src="<?php echo get_template_directory_uri(); ?>/assets/img/video-bg.png" alt="">
+                <div class="video-gallery__item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/video-1.png" alt="">
+                </div>
+                <div class="video-gallery__item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/video-2.png" alt="">
+                </div>
+                <div class="video-gallery__item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/video-3.png" alt="">
+                </div>
+                <div class="video-gallery__item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/video-4.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
